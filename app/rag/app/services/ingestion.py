28d41501d389d34fadae9a0dfc3d44f34744f564
ankush <raneankush93@gmail.com>
2026-06-18 23:20:02 +0530

    feat: multiple file ingesion

--- app/rag/app/services/ingestion.py
+++ app/rag/app/services/ingestion.py
@@ -1,5 +1,6 @@
 import pymupdf as pdf
-import docx 
+from fastapi import HTTPException
+from docx import Document
 from app.rag.app.services.chunker import chunk_text
 from app.rag.app.services.embeddings import embed_text
 from app.rag.app.services.storage import save_documents
@@ -7,20 +8,33 @@ from app.core.database import get_db
 import os
 from sqlalchemy.orm import Session
 from app.core.logger import logger
+from pathlib import Path
 
 
 
 
 def ingest_pdf(file_path:str,db:Session,user_id):
     #vars
-    file_type = ["pdf","docx","txt"]
-    file_name = os.path.basename(file_path)
-    doc = pdf.open(file_path)
+    file_type = [".pdf",".docx",".txt"]
     text = ""
+    ext = Path(file_path).suffix.lower()
+
+    if ext not in file_type:
+        raise HTTPException(status_code=400,detail="We only accept these ['.pdf','.docx','.txt'] file types")
+    if ext == ".pdf":
+        doc = pdf.open(file_path)
+        for page in doc:
+            text += page.get_text()
+    elif ext == ".docx":
+        doc = Document(file_path)
+        text = "\n".join(p.text for p in doc.paragraphs)
+    else:#txt
+        with open(file_path,"r",encoding="utf-8") as f:
+            text = f.read()
+
     
-    #fetching text from pdf
-    for page in doc:
-        text += page.get_text()
+    file_name = os.path.basename(file_path)
+
         
     if not text.strip():
         raise ValueError("No text found in pdf")
