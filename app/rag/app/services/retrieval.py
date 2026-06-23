a86458744e4bbd090b291233d86ef5e5697b58d2
ankush <raneankush93@gmail.com>
2026-06-23 21:25:24 +0530

    fix: filename and chunk_index on bm25/vector search

--- app/rag/app/services/retrieval.py
+++ app/rag/app/services/retrieval.py
@@ -10,23 +10,25 @@ from rank_bm25 import BM25Okapi
 def retrieve_chunks(wk_id:int,ques:str,db,k:int=5)->list[str]:
     embed_ques = embed_text([ques])[0]
 
-    query = text("SELECT id,content FROM chunks JOIN documents ON chunks.document_id=documents.id WHERE documents.workspace_id = wk_id ORDER BY chunks.embedding <=> CAST(:vector AS vector) LIMIT :k")
-    res = db.execute(query,{"vector":embed_ques,"k":k,"doc_id":wk_id})
+    query = text("SELECT chunks.id,chunks.content,chunks.chunk_index,documents.filename FROM chunks JOIN documents ON chunks.document_id=documents.id WHERE documents.workspace_id = :wk_id ORDER BY chunks.embedding <=> CAST(:vector AS vector) LIMIT :k")
+    res = db.execute(query,{"vector":embed_ques,"k":k,"wk_id":wk_id})
     rows = res.fetchall()
-    real_chunks = [{"id":row.id,"content":row.content} for row in rows]
+    real_chunks = [{"id":row.id,"filename":row.filename,"chunk_index":row.chunk_index,"content":row.content} for row in rows]
     return real_chunks
 
 def search_bm25(wk_id:int,db:Session,ques_query, k=20):
-    query = text("SELECT id,content FROM chunks JOIN documents ON chunks.document_id=documents.id WHERE documents.workspace_id = :wk_id")
+    query = text("SELECT chunks.id,chunks.content,chunks.chunk_index,documents.filename FROM chunks JOIN documents ON chunks.document_id=documents.id WHERE documents.workspace_id = :wk_id")
     res = db.execute(query,{"wk_id":wk_id})
     rows = res.fetchall()
+    filenames = [row.filename for row in rows]
+    chunk_index = [row.chunk_index for row in rows]
     real_chunks = [row.content for row in rows]
     chunks_id = [row.id for row in rows]
     tokenize = [chunk.lower().split() for chunk in real_chunks]
     bm25 = BM25Okapi(tokenize)
     scores = bm25.get_scores(ques_query.lower().split())
     scored = sorted(enumerate(scores), key=lambda x: x[1], reverse=True)[:k]
-    top_chunks = [{"id": chunks_id[i], "content": real_chunks[i]} for i, score in scored]
+    top_chunks = [{"id": chunks_id[i],"filename":filenames[i],"chunk_index":chunk_index[i],"content": real_chunks[i]} for i, score in scored]
     return top_chunks
 
 
@@ -37,16 +39,16 @@ def rrf_merge(vector_res, bm25_res, k=60, top_n=5):
     for i in range(n):
         chunk = bm25_res[i]
         save = 1/(k+i+1)
-        chunk_id[chunk["id"]]={"score":save, "content":chunk["content"]}
+        chunk_id[chunk["id"]]={"score":save, "content":chunk["content"], "filename":chunk["filename"], "chunk_index":chunk["chunk_index"]}
     for j in range(m):
         chunk = vector_res[j]
         save = 1/(k+j+1)
         if chunk["id"] in chunk_id:
-            chunk_id[chunk["id"]]={"score":chunk_id[chunk["id"]]["score"]+save,"content":chunk["content"]}
+            chunk_id[chunk["id"]]={"score":chunk_id[chunk["id"]]["score"]+save,"content":chunk["content"], "filename":chunk["filename"], "chunk_index":chunk["chunk_index"]}
         else:
-            chunk_id[chunk["id"]]={"score":save,"content":chunk["content"]}
+            chunk_id[chunk["id"]]={"score":save,"content":chunk["content"], "filename":chunk["filename"], "chunk_index":chunk["chunk_index"]}
     sorting = sorted(chunk_id.items(),key=lambda pair: pair[1]["score"],reverse=True)[:top_n]
-    final = [{"id": chunk_final[0], "content": chunk_final[1]["content"]}for chunk_final in sorting]
+    final = [{"id": chunk_final[0], "content": chunk_final[1]["content"], "filename":chunk_final[1]["filename"], "chunk_index":chunk_final[1]["chunk_index"]}for chunk_final in sorting]
     return final
 
 def hybrid_search(db:Session,wk_id:int,question:str):
