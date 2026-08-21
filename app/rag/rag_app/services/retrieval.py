d177e30e9ff9d2e29ed62e47ad66f150ad264771
ankush <raneankush93@gmail.com>
2026-08-22 00:22:04 +0530

    fix: hybrid search

--- app/rag/rag_app/services/retrieval.py
+++ app/rag/rag_app/services/retrieval.py
@@ -11,12 +11,12 @@ async def ques_embed(ques:str):
 async def retrieve_chunks(wk_id:int,embed_ques:str,db:AsyncSession,k:int=5)->list[str]:
 
     query = text("SELECT chunks.id,chunks.content,chunks.chunk_index,documents.filename FROM chunks JOIN documents ON chunks.document_id=documents.id WHERE documents.workspace_id = :wk_id ORDER BY chunks.embedding <=> CAST(:vector AS vector) LIMIT :k")
-    res = await db.execute(query,{"vector":vector_que,"k":k,"wk_id":wk_id})
+    res = await db.execute(query,{"vector":embed_ques,"k":k,"wk_id":wk_id})
     rows = res.fetchall()
     real_chunks = [{"id":row.id,"filename":row.filename,"chunk_index":row.chunk_index,"content":row.content} for row in rows]
     return real_chunks
 
-def hybrid_search(wk_id:int, embed_ques:str, db:AsyncSession,top_n: int=5, k: int=20)->list[dict]:
+async def hybrid_search(wk_id:int, embed_ques:str,ques:str, db:AsyncSession,top_n: int=5, k: int=20)->list[dict]:
     query = text("""
         WITH vector_results AS (
             SELECT 
@@ -30,8 +30,39 @@ def hybrid_search(wk_id:int, embed_ques:str, db:AsyncSession,top_n: int=5, k: in
             JOIN documents ON chunks.document_id = documents.id
             WHERE documents.workspace_id = :wk_id
             LIMIT :k
-        ),
+        ),bm25_results AS(
+            SELECT
+                chunks.id,
+                chunks.content,
+                chunks.chunk_index,
+                documents.filename,
+                ROW_NUMBER() OVER (ORDER BY paradedb.score(chunks.id) DESC)
+        AS rank
+            FROM chunks
+            JOIN documents ON chunks.document_id = documents.id
+            WHERE chunks.id @@@ paradedb.match('content', :question)
+                AND documents.workspace_id = :wk_id
+            LIMIT :k
+        ),rrf AS (
+                SELECT 
+                COALESCE(v.id, b.id)                   AS id,
+                COALESCE(v.content, b.content)         AS content,
+                COALESCE(v.filename, b.filename)       AS filename,
+                COALESCE(v.chunk_index, b.chunk_index) AS chunk_index,
+                COALESCE(1.0 / (60 + v.rank), 0)
+                + COALESCE(1.0 / (60 + b.rank),0)      AS rrf_score
+            FROM vector_results v 
+            FULL OUTER JOIN bm25_results b ON v.id = b.id
+        )
+        SELECT id, content, chunk_index, filename, rrf_score
+        FROM rrf
+        ORDER BY rrf_score DESC
+        LIMIT :top_n
     """)
+
+    res = await db.execute(query,{"wk_id":wk_id,"top_n":top_n,"k":k,"vector":embed_ques,"question":ques})
+    result = res.fetchall()
+    return result
     
 
     
